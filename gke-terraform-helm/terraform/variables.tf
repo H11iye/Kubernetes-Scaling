c89ab83be5386089531cbafe39eb70ace77b856f
H11iye <hamdi.iyed11@gmail.com>
2025-10-06 15:32:40 +0000

    Create terraform folder for IaC

---
+++ gke-terraform-helm/terraform/variables.tf
@@ -0,0 +1,28 @@
+variable "project_id" {
+ type = string
+
+}
+
+variable "region" {
+  type = string
+  default = "us-central1"
+}
+variable "zone" {
+  type = string
+  default = "us-central1-a"
+}
+
+variable "cluster_name" {
+  type = string
+  default = "gke-terraform-helm-cluster"
+}
+
+variable "node_count" {
+  type = number
+  default = 2
+}
+
+variable "machine_type" {
+  type = string
+  default = "e2-medium"
+}
