9ecba1f7d7a4c04fda98342f14849c00ee0700f1
H11iye <hamdi.iyed11@gmail.com>
2025-10-06 15:50:23 +0000

    Create the minimal for IaC for the quickstart

--- gke-terraform-helm/terraform/main.tf
+++ gke-terraform-helm/terraform/main.tf
@@ -1,3 +1,21 @@
 resource "google_container_cluster" "gke" {
-  name = var
+  name = var.cluster_name
+  location = var.region 
+  remove_default_node_pool = true
+  initial_node_count = 1
+  # TO DO - Add network and ip_allocation_policy etc
 }
+
+resource "google_container_node_pool" "primary_nodes" {
+  name = "primary-pool"
+  location = var.region
+  cluster = google_container_cluster.gke.name
+  node_count = var.node_count
+  node_config {
+    machine_type = var.machine_type
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+}
+
